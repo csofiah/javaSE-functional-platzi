28dea2334c7c77472fe33c99142ce79065ea6c20
Sofia Hernandez <csofiah>
2021-10-25 20:35:36 -0500

    example of optionals

--- src/main/java/com/platzi/functional/_14_optionals/DemoOptionals.java
+++ src/main/java/com/platzi/functional/_14_optionals/DemoOptionals.java
@@ -1,12 +1,10 @@
 package com.platzi.functional._14_optionals;
 
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 public class DemoOptionals {
 
+    //platzi
     public static void main(String[] args) {
         List<String> names = getNames();
         if(names == null){
@@ -17,13 +15,42 @@ public class DemoOptionals {
         if(optionalNames.isPresent()){
 
         }
+        //ifPresent(Consumer toma un dato pero no retirna resultado)
         optionalNames.ifPresent(namesValue -> namesValue.forEach(System.out::println));
         //flatMap va recibir un elemento y va utilizar su valor
         //optionalNames.flatMap()
 
         Optional<String> valuePlayer = optionalValuablePlayer();
         String valuablePlayerName = valuePlayer.orElseGet(() -> "No player");
+
+        //adictosaltrabajo
+
+        Optional<String> optionalNames2 = validEmptyString(getNamesWithValues());
+        optionalNames2.filter(s -> !s.isEmpty())
+                .map(s -> s.toUpperCase(Locale.ROOT))
+                .orElse("nombres vacios");
+
+
+
+
+
     }
+    static Optional<String> validEmptyString(List<String> names){
+        if(names == null || names.isEmpty()){
+            return Optional.empty();
+        }
+        return Optional.of(Arrays.toString(names.toArray()));
+    }
+
+    static List<String> getNamesWithValues(){
+        List<String> list = new LinkedList<>();
+        list.add("ana");
+        list.add("pedro");
+        list.add("david");
+
+        return list;
+    }
+
     static List<String> getNames(){
        List<String> list = new LinkedList<>();
 
@@ -48,4 +75,42 @@ public class DemoOptionals {
         //si no encuentra nada
         return Optional.empty();
     }
+
+    static double getDurationOfAlbumWithName(String name){
+        return getAlbum(name)
+                .flatMap((album) -> getAlbumTracks(getName()))
+                .map((tracks) -> getTracksDuration(getName()))
+                .orElse(0.0);
+    }
+
+    //other example with orElseGet //is for execute the function
+   /* private static Album getAlbum(String albumName) {
+        Album album = getAlbumFromCache(albumName)
+                .orElseGet(() -> getAlbumFromDisk(albumName));
+        return album;
+    }*/
+
+    static Optional<String> getAlbum(String name){
+        return Optional.of(name.concat("Album"));
+    }
+
+    static Optional<List<String>>  getAlbumTracks(String albumName){
+        List<String> listTracks = new LinkedList<>();
+        listTracks.add("song1");
+        listTracks.add("song2");
+        listTracks.add("song3");
+
+       List<String> emptyTracks = new ArrayList<>();
+
+        return Optional.of(listTracks);
+
+
+    }
+    static String getName(){
+        return "Album1";
+    }
+    static double getTracksDuration(String nameTrack){
+        return 1.5;
+    }
+
 }
--- src/main/java/com/platzi/functional/_14_optionals/OptionalBasicExample_myyong.java
+++ src/main/java/com/platzi/functional/_14_optionals/OptionalBasicExample_myyong.java
@@ -4,7 +4,69 @@ import java.util.Optional;
 
 public class OptionalBasicExample_myyong {
 
+    //https://mkyong.com/java8/java-8-optional-in-depth/
     public static void main(String[] args) {
+
+      //  optionalBasicExample();
+       // optionalMapFlapMapExample();
+      //  optionalFilterExample();
+      //  optionalIfPresentExample();
+        optionalOrElseExample();
+    }
+
+    private static void optionalOrElseExample() {
+        Optional<String> gender = Optional.of("MALE");
+        Optional<String> emptyGender = Optional.empty();
+
+        System.out.println(gender.orElse("NA"));
+        System.out.println(emptyGender.orElse("NA"));
+
+        System.out.println(gender.orElseGet(() -> "NA"));
+        System.out.println(emptyGender.orElseGet(() -> "NA"));
+
+
+    }
+
+    private static void optionalIfPresentExample() {
+        Optional<String> gender = Optional.of("MALE");
+        Optional<String> emptyGender = Optional.empty();
+
+        if(gender.isPresent())
+            System.out.println("value is " + gender.get());
+        else
+            System.out.println("value is not available");
+
+        gender.ifPresent(g -> System.out.println("gender is available"));
+        emptyGender.ifPresent(g -> System.out.println("no imprime nada"));
+    }
+
+    private static void optionalFilterExample() {
+        Optional<String> gender = Optional.of("MALE");
+        Optional<String> emptyGender = Optional.empty();
+
+        //filter on Optional
+        System.out.println(gender.filter(g -> g.equals("male")));
+        System.out.println(gender.filter(s -> s.equalsIgnoreCase("male")));
+        System.out.println(emptyGender.filter(g -> g.equalsIgnoreCase("MALE")));
+    }
+
+    private static void optionalMapFlapMapExample(){
+
+        Optional<String> nonEmptyGender2 = Optional.of("female");
+        Optional<String> emptyGender2 = Optional.empty();
+
+        System.out.println("Nom empty Optional " + nonEmptyGender2.map(String::toUpperCase));
+        System.out.println("Empty Optional " + emptyGender2.map(String::toUpperCase));
+
+        Optional<Optional<String>> nonEmptyOptionalGender2 = Optional.of(Optional.of("male"));
+        System.out.println("Optional value " + nonEmptyOptionalGender2);
+        System.out.println("Optional.map     :: " + nonEmptyOptionalGender2.map(gender -> gender.map(String::toUpperCase)));
+        System.out.println("Optional.flatmap " + nonEmptyOptionalGender2.flatMap(gender -> gender.map(String::toUpperCase)));
+
+
+    }
+
+    private static void optionalBasicExample() {
         Optional<String> gender = Optional.of("male");
         String answer1 = "Yes";
         String answer2 = null;
@@ -17,7 +79,7 @@ public class OptionalBasicExample_myyong {
         System.out.println("ofNullable on Empty Optional: " + Optional.ofNullable(answer2));
 
         //java.lang.NullPointerException
-       // System.out.println("ofNullable on Non-Empty Optional: " + Optional.of(answer2));
+        // System.out.println("ofNullable on Non-Empty Optional: " + Optional.of(answer2));
 
         //--------------------------------------
         Optional<String> nonEmptyGender = Optional.of("male");
@@ -25,8 +87,7 @@ public class OptionalBasicExample_myyong {
 
         Optional<Optional<String>> nonEmptyOptionalGender = Optional.of(Optional.of("male"));
         System.out.println("Optional value :: "+ nonEmptyOptionalGender);
-       // System.out.println("Optional.map :: "+ nonEmptyOptionalGender.map(gender -> gender.map(String::toUpperCase)));
-       // System.out.println("Optional.flatmap :: "+ nonEmptyOptionalGender.flatMap(gender -> gender.map(String::toUpperCase)));
-
+        // System.out.println("Optional.map :: "+ nonEmptyOptionalGender.map(gender -> gender.map(String::toUpperCase)));
+        // System.out.println("Optional.flatmap :: "+ nonEmptyOptionalGender.flatMap(gender -> gender.map(String::toUpperCase)));
     }
 }
